36c54f6f38f58241072a5ff574a75104c26cb39d
Simon Jones <simonp.jones@ntlworld.com>
2025-06-27 14:57:28 +0100

    User story 1: get products from specified endpoint
    Pulling products from the product access class rather than a static field in products. Added unit tests, and some integration tests in order to cover the product access behaviour.

---
+++ Greggs.Products.IntegrationTests/ProductIntegrationTests.cs
@@ -0,0 +1,59 @@
+using FluentAssertions;
+using Greggs.Products.Api.Controllers;
+using Greggs.Products.Api.DataAccess;
+using Greggs.Products.Api.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Greggs.Products.IntegrationTests
+{
+    // I've created this because in reality product access would be calling an api or a database etc. With the unit tests,
+    // the functionality in that class is hidden. We can test for real via an integration test.
+    public class ProductIntegrationTests
+    {
+        Mock<ILogger<ProductController>> logger = new Mock<ILogger<ProductController>>();
+
+        [Fact]
+        public void TestFirstPage()
+        {
+            var productsAccess = new ProductAccess();
+            var controller = new ProductController(logger.Object, productsAccess);
+
+            var results = controller.Get(0, 3);
+
+            results.Should().BeEquivalentTo(new List<Product> {
+                new() { Name = "Sausage Roll", PriceInPounds = 1m },
+                new() { Name = "Vegan Sausage Roll", PriceInPounds = 1.1m },
+                new() { Name = "Steak Bake", PriceInPounds = 1.2m },
+            });
+        }
+
+        [Fact]
+        public void TestLaterPage()
+        {
+            var productsAccess = new ProductAccess();
+            var controller = new ProductController(logger.Object, productsAccess);
+
+            var results = controller.Get(3, 2);
+
+            results.Should().BeEquivalentTo(new List<Product> {
+                new() { Name = "Yum Yum", PriceInPounds = 0.7m },
+                new() { Name = "Pink Jammie", PriceInPounds = 0.5m },
+            });
+        }
+
+        [Fact]
+        public void SelectionExceedsNumberOfProducts_ReturnWhatWeCan()
+        {
+            var productsAccess = new ProductAccess();
+            var controller = new ProductController(logger.Object, productsAccess);
+
+            var results = controller.Get(6, 2);
+
+            results.Should().BeEquivalentTo(new List<Product> {
+                new() { Name = "Bacon Sandwich", PriceInPounds = 1.95m },
+                new() { Name = "Coca Cola", PriceInPounds = 1.2m }
+            });
+        }
+    }
+}
---
+++ Greggs.Products.UnitTests/ProductRetrievalTests.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using Greggs.Products.Api.Controllers;
+using Greggs.Products.Api.DataAccess;
+using Greggs.Products.Api.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Greggs.Products.UnitTests;
+
+public class ProductRetrievalTests
+{
+    Mock<ILogger<ProductController>> logger = new Mock<ILogger<ProductController>>();
+    Mock<IDataAccess<Product>> productAccess = new Mock<IDataAccess<Product>>();
+
+    [Fact]
+    public void RetrievesExpectedProducts()
+    {
+        var pageStart = 0;
+        var pageSize = 2;
+
+        var mockProducts = new List<Product> { new Product { Name = "Product 1" }, new Product { Name = "Product2" } };
+
+        productAccess.Setup(x => x.List(pageStart, pageSize)).Returns(mockProducts);
+
+        var controller = new ProductController(logger.Object, productAccess.Object);
+
+        var result = controller.Get(pageStart, pageSize);
+
+        result.Should().BeEquivalentTo(mockProducts);
+    }
+
+    [Fact]
+    public void CreatesALogEntry()
+    {
+        var controller = new ProductController(logger.Object, productAccess.Object);
+
+        controller.Get(2, 4);
+
+        // Only checking for a log call of the correct level, not the content. Wouldn't want to fail tests if I tweaked the message
+        logger.Verify(
+        x => x.Log(
+            It.Is<LogLevel>(l => l == LogLevel.Information),
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => true),
+            It.IsAny<Exception>(),
+            It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)));
+    }
+
+    // Maybe would prefer to return a bad status code. I've gone with empty products for now
+    [Theory]
+    [InlineData(-1, 3)]
+    [InlineData(1, -2)]
+    public void NegativePageInfo_ReturnEmptyProducts(int pageStart, int pageSize)
+    {
+        var mockProducts = new List<Product> { new Product { Name = "Product 1" }, new Product { Name = "Product2" } };
+
+        productAccess.Setup(x => x.List(It.IsAny<int>(), It.IsAny<int>())).Returns(mockProducts);
+
+        var controller = new ProductController(logger.Object, productAccess.Object);
+
+        var result = controller.Get(-1, 3);
+
+        result.Should().BeEmpty();
+    }
+}
